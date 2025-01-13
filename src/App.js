731480c4227828f5aba8e22afecd7a0d4c942ffa
yesboo <nobut125@gmail.com>
2025-01-13 10:37:32 +0900

    4-12

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import '@aws-amplify/ui-react/styles.css';
 import { Header } from './ui-components';
 import { useState } from "react";
 
-
 function App() {
   const [count, setCount] = useState(0);
+  const [msg, setMsg] = useState("");
+  const [msgs, setMsgs] = useState([]);
 
   const onClick = () =>{
     setCount(count + 1);
+    setMsgs([
+      "Hello, " + msg + "!",
+      "こんにちは、" + msg + "さん。"
+    ]);
+  }
+
+  const onChange = (event) => {
+    setMsg(event.target.value);
   }
 
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これは、UIコンポーネントを利用した表示です。</p>
-      <Hello message = "サンプルメッセージです。" type="primary"/>
-      <Hello message = "表示タイプも変更か。" type="dark"/>
+      <div className="mx-0 my-3 row">
+        <input type="text" className = "form-control col" onChange={onChange}/ >
+        <button className="btn btn-primary col-2" onClick={onClick}> Click</button>
+      </div>
+      <Hello message = {msgs[0]} type="primary"/>
+      <Hello message = {msgs[1]} type="dark"/>
       <Now />
       <div className ="alert alert-primary">Count : {count} </div>
       <button className= "btn btn-primary" onClick={onClick} > Click me!!</button>
