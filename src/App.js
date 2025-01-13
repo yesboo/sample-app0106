52455380753da5bb7cd3a1308b9e9048a4177a78
yesboo <nobut125@gmail.com>
2025-01-13 11:54:23 +0900

    5-1

--- src/App.js
+++ src/App.js
@@ -2,74 +2,58 @@
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 import { Header } from './ui-components';
-import { useState, useEffect } from "react";
+import { Amplify, Auth } from 'aws-amplify';  // 修正
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import aws_exports from './aws-exports';
+
+const content1 = <p>タブ1のコンテンツ</p>;  // ①
+const content2 = <p>タブ2のコンテンツ</p>;  // ②
+const content3 = <p>タブ3のコンテンツ</p>;  // ③
+const content4 = <p>タブ4のコンテンツ</p>;  // ④
+Amplify.configure(aws_exports);
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [input, setInput] = useState("");
-  const [msg, setMsg] = useState(input);
-  const [msgs, setMsgs] = useState(msg);
-  const data = [
-    ["おやすみ、", "..."],
-    ["おはよう、", "！"],
-    ["こんにちは、", "さん。"],
-    ["こんばんは、", "さん。"]
-  ]
-
-  const onClick = () =>{
-    setCount(count + 1);
-    setMsg(input);
-  }
-
-  const onChange = (event) => {
-    setInput(event.target.value);
-  }
-
-  useEffect(()=>{
-    if (msg==""){
-      setMsgs("no massage.");
-    }else{
-      const h = Math.floor(new Date().getHours() / 6) ;
-      setMsgs(data[h][0] + msg + data[h][1]);
-    }
-  }, [msg]);
-
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これは、UIコンポーネントを利用した表示です。</p>
-      <div className="mx-0 my-3 row">
-        <input type="text" className = "form-control col" onChange={onChange}/ >
-        <button className="btn btn-primary col-2" onClick={onClick}>Click</button>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+        <a href="#tab1" className="nav-link active" 
+          data-bs-toggle="tab">List</a>
+        </li>
+        <li className="nav-item">
+        <a href="#tab2" className="nav-link" 
+          data-bs-toggle="tab">Create</a>
+        </li>
+        <li className="nav-item">
+        <a href="#tab3" className="nav-link" 
+          data-bs-toggle="tab">Update</a>
+        </li>
+        <li className="nav-item">
+        <a href="#tab4" className="nav-link" 
+          data-bs-toggle="tab">Delete</a>
+        </li>
+      </ul>
+      <div className="tab-content">
+        <div id="tab1" className="my-2 tab-pane active">
+          { content1 }
+        </div>
+        <div id="tab2" className="my-2 tab-pane">
+          { content2 }
+        </div>
+        <div id="tab3" className="my-2 tab-pane">
+          { content3 }
+        </div>
+        <div id="tab4" className="my-2 tab-pane">
+          { content4 }
+        </div>
       </div>
-      <Hello message = {msgs} type="primary"/>
-      <Now />
-      <div className ="alert alert-primary">Count : {count} </div>
-      <button className= "btn btn-primary" onClick={onClick} > Click me!!</button>
+        <a className="btn btn-primary" href="." onClick={async () => { await Amplify.Auth.signOut(); }}>
+          Sign Out
+        </a>
     </div>
   );
 }
 
-function Hello(props) {
-  return (
-    <p className= {"alert alert-" + props.type}>
-      {props.message }
-    </p>
-  );
-}
-
-function Now() {
-  return (
-    <p className="bg-secondary text-dark bg-opacity-25 p-3 my-3">
-      現在は、{
-        new Date().getHours()
-      }時です。
-    </p>
-  );
-}
-
-//function onClick(){
-//  alert("クリックした。");
-//}
-
-export default App;
+export default withAuthenticator(App);
