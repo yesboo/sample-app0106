d25d49c63daba01f35058f9b51a8172b91c7aec4
yesboo <nobut125@gmail.com>
2025-01-13 11:02:41 +0900

    4-3

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 import { Header } from './ui-components';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [msg, setMsg] = useState("");
-  const [msgs, setMsgs] = useState([]);
+  const [input, setInput] = useState("");
+  const [msg, setMsg] = useState(input);
+  const [msgs, setMsgs] = useState(msg);
+  const data = [
+    ["おやすみ、", "..."],
+    ["おはよう、", "！"],
+    ["こんにちは、", "さん。"],
+    ["こんばんは、", "さん。"]
+  ]
 
   const onClick = () =>{
     setCount(count + 1);
-    setMsgs([
-      "Hello, " + msg + "!",
-      "こんにちは、" + msg + "さん。"
-    ]);
+    setMsg(input);
   }
 
   const onChange = (event) => {
-    setMsg(event.target.value);
+    setInput(event.target.value);
   }
+
+  useEffect(()=>{
+    if (msg==""){
+      setMsgs("no massage.");
+    }else{
+      const h = Math.floor(new Date().getHours() / 6) ;
+      setMsgs(data[h][0] + msg + data[h][1]);
+    }
+  }, [msg]);
 
   return (
     <div className="py-4">
@@ -27,10 +40,9 @@ function App() {
       <p>※これは、UIコンポーネントを利用した表示です。</p>
       <div className="mx-0 my-3 row">
         <input type="text" className = "form-control col" onChange={onChange}/ >
-        <button className="btn btn-primary col-2" onClick={onClick}> Click</button>
+        <button className="btn btn-primary col-2" onClick={onClick}>Click</button>
       </div>
-      <Hello message = {msgs[0]} type="primary"/>
-      <Hello message = {msgs[1]} type="dark"/>
+      <Hello message = {msgs} type="primary"/>
       <Now />
       <div className ="alert alert-primary">Count : {count} </div>
       <button className= "btn btn-primary" onClick={onClick} > Click me!!</button>
